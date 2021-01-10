64c4e591b1b759080ba2bfcc4243bcde280e1065
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-01-10 19:08:26 +0530

    Config DBCP in listener/MyContextListener.java

--- src/main/java/lk/ijse/dep/web/listener/MyContextListener.java
+++ src/main/java/lk/ijse/dep/web/listener/MyContextListener.java
@@ -5,6 +5,10 @@ package lk.ijse.dep.web.listener;
  * @since : 10/01/2021
  **/
 
+import lk.ijse.dep.web.commonConstant.CommonConstant;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -12,6 +16,7 @@ import javax.servlet.http.HttpSessionAttributeListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import javax.servlet.http.HttpSessionBindingEvent;
+import java.sql.SQLException;
 
 @WebListener()
 public class MyContextListener implements ServletContextListener{
@@ -28,7 +33,16 @@ public class MyContextListener implements ServletContextListener{
          initialized(when the Web application is deployed). 
          You can initialize servlet context related data here.
       */
-        System.out.println("Context is being initialized");
+//        System.out.println("Context is being initialized");
+
+        BasicDataSource bds = new BasicDataSource();
+        bds.setUsername(CommonConstant.MYSQL_USER_NAME);
+        bds.setPassword(CommonConstant.MYSQL_PASSWORD);
+        bds.setUrl(CommonConstant.MYSQL_URL);
+        bds.setInitialSize(5);
+        bds.setMaxTotal(5);
+        ServletContext ctx = sce.getServletContext();
+        ctx.setAttribute("cp",bds);
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
@@ -36,7 +50,15 @@ public class MyContextListener implements ServletContextListener{
          (the Web application) is undeployed or 
          Application Server shuts down.
       */
-        System.out.println("Context is being destroyed");
+//        System.out.println("Context is being destroyed");
+
+        BasicDataSource cp = (BasicDataSource) sce.getServletContext().getAttribute("cp");
+
+        try {
+            cp.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
 }
